6d8bfb250d7d5c3c4c9fe7e6bef4e1baaf383fb8
Roman <roman.kutselepa@gmail.com>
2022-05-18 12:55:55 +0300

    Homework

---
+++ 101_homeworks/005_functions/001_functions.py
@@ -0,0 +1,35 @@
+"""
+Под каждым комментарием нужно написать одну функцию
+Задание в комментарии
+input - параметр который функция получает
+output - параметр который функция возвращает
+"""
+
+
+# Write a function that takes a list of numbers
+# And returns same list but numbers multiplied by 2
+# input: list of numbers
+# output: list of numbers multiplied by 2
+
+
+# Write a function that takes a list of numbers
+# And returns same list but numbers multiplied by 4
+# USE PREVIOUS FUNCTION TO CREATE THIS ONE
+# input: list of numbers
+# output: list of numbers multiplied by 4
+
+
+# Write a function that will check if users input is palindrome
+# "Able was I ere I saw Elba" is a palindrome!!!
+# input: nothing
+# output: boolean
+
+
+# Write a function that prints a list of squared numbers from 1 to 30
+# input: nothing
+# output: nothing
+
+
+# Write a function that multiplies all numbers in a list provided by user
+# input: nothing
+# output: float of multiplied numbers
