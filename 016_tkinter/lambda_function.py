7a5b367dcc39018676daf3a2082de215a2125f6a
mrArtful <roman.kutselepa@gmail.com>
2022-06-22 15:15:41 +0300

    Tkinter

---
+++ 016_tkinter/lambda_function.py
@@ -0,0 +1,8 @@
+# some_list = [[1, 2, 12], [3, 7, 9], [4, 13, 3], [2, 1, 1]]
+#
+#
+# some_list.sort(key=lambda x: x[2])
+# print(some_list)
+y = lambda x, a, b: x * a ** b
+# print(y())
+print(y(2, 4, 6))
